3323a66e41092b22ec371a88b9a8905d9cec439b
molos <mateusz.olos@locon.pl>
2024-11-26 15:06:02 +0100

    1.5.3

--- android-ago/src/com/github/curioustechizen/ago/DateTextConfiguration.java
+++ android-ago/src/com/github/curioustechizen/ago/DateTextConfiguration.java
@@ -1,3 +1,17 @@
-public class DateTextConfiguration(String now) {
-    this.now = now;
+package com.github.curioustechizen.ago;
+
+public class DateTextConfiguration {
+    private String now;
+
+    public DateTextConfiguration(String now) {
+        this.now = now;
+    }
+
+    public String getNow() {
+        return now;
+    }
+
+    public void setNow(String now) {
+        this.now = now;
+    }
 }
